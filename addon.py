ab78bfad9ea201db2730299c3a61e7e7b71d4f62
Peziman <44964725+Peziman@users.noreply.github.com>
2019-12-03 10:04:32 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -1,3 +1,37 @@
 import xbmc
 import xbmcgui
 import xbmcaddon
+
+# Get global paths
+addon = xbmcaddon.Addon(id='plugin.program.navigation2020')
+addonpath = addon.getAddonInfo('path').decode("utf-8")
+
+#control
+BACK_BUTTON  = 1100
+
+
+
+class Navigation2020(xbmcgui.WindowXMLDialog):
+    def onInit(self):
+        Nvigation2020.button_back=self.getControl(BACK_BUTTON)
+      
+    def onAction(self):
+        pass
+    
+    def onClick(self, controlID):
+        
+        if controlID == BACK_BUTTON:
+            self.close()
+           
+    def onFocus(self, controlID):
+        pass
+    
+    def onControl(self, controlID):
+        pass
+    
+navigation2020dialog = Navigation2020("navigation2020.xml", addonpath, 'default', '720')
+
+navigation2020dialog.doModal()
+del navigation2020dialog
+          
+
